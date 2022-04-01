4af0331e1e9cc22e5e29b5877bebf441bb646e93
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2022-04-01 14:10:29 +0600

    final

--- src/test/java/ru/netology/testMode/AuthTest.java
+++ src/test/java/ru/netology/testMode/AuthTest.java
@@ -1,2 +1,98 @@
-package ru.netology.testMode;public class AuthTest {
+package ru.netology.testMode;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import ru.netology.Data.DataGenerator;
+import ru.netology.Data.RegistrationInfo;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.Data.DataGenerator.Registration.*;
+
+
+public class AuthTest {
+
+
+    @Test
+    void shouldLoginActiveUser() {
+        //Активный пользователь
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("active");
+        sendRequest(user);
+        $x("//input[@type='text']").val(user.getLogin());
+        $x("//input[@type='password']").val(user.getPassword());
+        $x("//span[@class='button__text']").click();
+        $x("//*[contains(text(), 'Личный кабинет')]").should(Condition.visible);
+    }
+
+    @Test
+    void shouldLoginBlockedUser() {
+        //Заблокированный пользователь
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("blocked");
+        sendRequest(user);
+        $x("//input[@type='text']").val(user.getLogin());
+        $x("//input[@type='password']").val(user.getPassword());
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']").should(Condition.visible);
+
+
+    }
+
+    @Test
+    void shouldWrongLogin() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("blocked");
+        sendRequest(user);
+        $x("//input[@type='text']").val(getRandomLogin()); //Введен рандомный логин
+        $x("//input[@type='password']").val(user.getPassword());
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']").should(Condition.visible);
+
+
+    }
+
+    @Test
+    void shouldWrongPassword() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("blocked");
+        sendRequest(user);
+        $x("//input[@type='text']").val(user.getLogin());
+        $x("//input[@type='password']").val(getRandomPassword());  //Введен рандомный паспорт
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']").should(Condition.visible);
+
+
+    }
+
+    @Test
+    void shouldNoPassword() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("blocked");
+        sendRequest(user);
+        $x("//input[@type='text']").val(user.getLogin());
+        // воод паспорта отсутствует
+        $x("//span[@class='button__text']").click();
+        $("[data-test-id='password'].input_invalid .input__sub").should(Condition.visible);
+
+
+    }
+
+    @Test
+    void shouldNoLogin() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        RegistrationInfo user = DataGenerator.Registration.getRegisteredUser("blocked");
+        sendRequest(user);
+        //Ввод логина отсутствует
+        $x("//input[@type='password']").val(user.getPassword());
+        $x("//span[@class='button__text']").click();
+        $("[data-test-id='login'].input_invalid .input__sub").should(Condition.visible);
+    }
 }
